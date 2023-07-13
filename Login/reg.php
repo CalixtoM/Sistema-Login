4efa55d35313ff902db4f6d24cea9734f5146c62
Matheus Calixto <51165259+CalixtoM@users.noreply.github.com>
2023-07-13 11:28:28 -0300

    mudança de layout
    Adição de verificação de força da senha, novo design, etc

--- Login/reg.php
+++ Login/reg.php
@@ -30,40 +30,161 @@ $sql = "INSERT INTO tb_usuario VALUES (null, '".$_POST['nm']."', '".$_POST['em']
 
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-	<!-- Links Bootstrap-->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 
-	<title>Login</title>
-</head>
-<body>
-<form method="post">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-4">
-				<h4>Nome:</h4><input type="text" name="nm" class="form-control">
-			</div>
-			<div class="col-sm-4">
-				<h4>Email:</h4><input type="email" name="em" class="form-control">
-			</div>
-			<div class="col-sm-4">
-				<h4>Senha:</h4><input type="password" name="pw" class="form-control">
-			</div>
-		</div><br><br>
-		<div class="row" id="c">
-			<input type="submit" name="bt" class="btn btn-success" value="registrar">
-		</div>
-	</div>
-</form>
+    <title></title>
+  </head>
 
-</body>
+  <script>
+      function verificaForcaSenha() 
+      {
+      	var numeros = /([0-9])/;
+      	var alfabeto = /([a-zA-Z])/;
+      	var chEspeciais = /([~,!,@,#,$,%,^,&,*,-,_,+,=,?,>,<])/;
+
+      	if($('#inputPassword').val().length<6 || document.getElementById("inputPassword").value == ""){
+      		$('#password-status').html("<span style='color:red'>Fraco, insira no mínimo 6 caracteres</span>");
+      	}else {  	
+      		if($('#inputPassword').val().match(numeros) && $('#inputPassword').val().match(alfabeto) && $('#inputPassword').val().match(chEspeciais)){            
+      			$('#password-status').html("<span style='color:green'><b>Forte</b></span>");
+      		} else {
+      			$('#password-status').html("<span style='color:orange'>Médio, insira um caracter especial</span>");
+      		}
+      	}
+
+      }
+      </script>	
+
+  <body class="text-center">
+    <div id="formulario" class="container text-center">
+      <!--<form class="form-signin" method="post">--> 
+      <div class="row" class="line">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6">
+          <h1 id="title" class="h3 mb-3 font-weight-normal" >Registro</h1>
+        </div>
+      </div>
+
+      <div class="row" class="line">
+        <div class="col-sm-12">
+          <label for="inputEmail" class="sr-only">Nome</label>
+          <input type="email" id="inputEmail" name="nome" class="form-control" placeholder="Nome" required="" autofocus="">
+        </div>
+      </div>
+
+      <div class="row" class="line">
+        <div class="col-sm-12">
+          <label for="inputEmail" class="sr-only">Email</label>
+          <input type="email" id="inputEmail" name="em" class="form-control" placeholder="E-mail" required="" autofocus="">
+        </div>
+      </div>
+
+      <div class="row" class="line">
+        <div class="col-sm-12">
+          <label for="inputPassword" class="sr-only">Senha</label>
+          <input type="password" name="password" id="inputPassword"  class="form-control" placeholder="Senha" required="" minlength="6" maxlength="12" onkeydown="verificaForcaSenha();">
+        </div>
+      </div>
+
+      <div class="row" class="line">
+        <div class="col-sm-12">
+          <span id="password-status"></span>
+        </div>
+      </div>     
+
+      <div class="row" class="line">
+        <div class="col-sm-12">
+          <a href="index.php" id="r"><p>Login</p></a>
+        </div>
+      </div>
+      
+      <div class="row" class="line">
+        <div class="col-sm-12">
+          <input class="btn btn-lg btn-primary btn-block" id="button" type="submit" value="Registrar">
+        </div>
+      </div>
+
+      <div class="row" class="line">
+        <div class="col-sm-12">
+          <script type="text/javascript">
+            const date = new Date();
+            console.log(date);
+
+            let year = date.getFullYear();
+            console.log(year);
+          </script>
+
+          <script>
+            document.write("<p class='mt-5 mb-3 text-muted' style='color: white;'>© 2019-"+year+"</p>")
+          </script>
+        </div>
+      </div>
+
+        <!-- <?php echo '<p class="mt-5 mb-3 text-muted" style="color: white;">© 2019-'.date('Y').'</p>'  ?>--> 
+      </form>
+    </div>
+  </body>
+
 </html>
 
+
 <style type="text/css">
-	#c{
-	}
+
+  html {
+    position:fixed;
+    width:100%;
+    height:100%;
+  }
+
+  body{
+    background: rgb(2,0,36);
+    background: linear-gradient(141deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
+    min-height: 100%;
+  }
+
+
+  #title{
+    color: black;
+    padding-top: 10%;
+  }
+
+  #formulario{
+    margin-top: 10%;
+    width: 400px;
+    height: 100%;
+    background-color: #DCDBFC;
+    color: white;
+    box-shadow: 2px 2px 4px #000000;
+    border-radius: 1%;
+  }
+
+  #inputEmail, #inputPassword{
+    margin-bottom: 5%;
+  }
+
+  h1{
+    color: white;
+  }
+
+  #button{
+    background-color: #00d4ff;
+    border-color: #00d4ff;
+  }
+
+  #button:hover{
+    background-color: #009dbd;
+    border-color: #009dbd;
+  }
+
+  /*#r{
+    color: darkred;
+  }*/
+
 </style>
